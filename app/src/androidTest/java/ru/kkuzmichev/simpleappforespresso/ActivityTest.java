83b17d61e12e7ab9e33fa8fadcfa74d3af35f94b
Serg130618 <ssa0618@yandex.ru>
2024-01-19 19:26:57 +0300

    first commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ActivityTest.java
@@ -0,0 +1,16 @@
+//package ru.kkuzmichev.simpleappforespresso;
+//
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+//import androidx.test.rule.ActivityTestRule;
+//
+//import org.junit.Rule;
+//import org.junit.runner.RunWith;
+//
+//
+//@RunWith(AndroidJUnit4.class)
+//public class ActivityTest {
+//    @Rule
+//
+//
+//
+//}
